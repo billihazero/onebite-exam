a8c0ad07396e2f7810a309da325dd33494a61b72
billihazero <billihazero@gmail.com>
2025-10-29 09:44:13 +0900

    feat: 초기 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { Route, Routes } from "react-router";
 import "./App.css";
-
+import CounterPage from "./pages/counter-page";
 function App() {
-  return <>hello world</>;
+  return (
+    <Routes>
+      <Route path="/" element={<div>Home</div>} />
+      <Route path="counter" element={<CounterPage />} />
+      <Route path="/sign-in" element={<div>SignIn</div>} />
+      <Route path="/sign-up" element={<div>SignUp</div>} />
+    </Routes>
+  );
 }
 
 export default App;
