febfa514f6069a627e8c7f0847ffb55f33ed237c
billihazero <billihazero@gmail.com>
2025-10-29 16:48:56 +0900

    feat: zustand 활용 todolist 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router";
 import "./App.css";
 import CounterPage from "./pages/counter-page";
+import TodoListPage from "./pages/todo-list-page";
 function App() {
   return (
     <Routes>
       <Route path="/" element={<div>Home</div>} />
       <Route path="counter" element={<CounterPage />} />
+      <Route path="todo" element={<TodoListPage />} />
       <Route path="/sign-in" element={<div>SignIn</div>} />
       <Route path="/sign-up" element={<div>SignUp</div>} />
     </Routes>
